3d6538024923407f959137c62765a2ab97ccaaf6
Mason Rubik <mrubik@ualberta.ca>
2020-01-19 13:59:41 -0700

    Added skeleton structure for activity analysis

---
+++ src/analysers/activity_analyser.py
@@ -0,0 +1,8 @@
+from src.utilities.utilities import read_from_file
+import regex as re
+
+def analyse_sleep_data():
+    masterText = read_from_file('./Data/Activity/Master.txt')
+
+    # TODO: Parse
+    # TODO: Analyse
